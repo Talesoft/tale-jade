7f12e4ddbea185d02eefbbee420ac33b50f8e659
Torben Köhn <t.koehn@outlook.com>
2015-10-07 00:47:13 +0200

    Fixed PHP filters

--- src/Tale/Jade/Filter.php
+++ src/Tale/Jade/Filter.php
@@ -8,13 +8,23 @@ class Filter
     public static function wrapTag($tag, Node $node, $indent, $newLine)
     {
 
-        return "<$tag>".$newLine.self::filterPlain($node, $indent, $newLine).$indent."</$tag>";
+        return "<$tag>".$newLine.self::filterPlain($node, $indent, $newLine).$indent."</$tag>".$newLine;
     }
 
     public static function wrapCode(Node $node, $indent, $newLine)
     {
 
-        return "<?php ".$newLine.self::filterPlain($node, $indent, $newLine).$indent."?>";
+        $strlen = function_exists('mb_strlen') ? 'mb_strlen' : 'strlen';
+        $text = self::filterPlain($node, $indent, $newLine);
+
+        //Make sure to have it correctly wrapped in php tags
+        if (strpos($text, '<?php') !== 0)
+            $text = '<?php '.$newLine.$text;
+
+        if (strpos($text, '?>') !== $strlen($text) - 1)
+            $text .= $indent.'?>'.$newLine;
+
+        return $text;
     }
 
 
@@ -24,7 +34,7 @@ class Filter
         return implode($newLine, array_map(function($line) use($indent, $newLine) {
 
             return $indent.trim($line);
-        }, preg_split("/\r?\n/", $node->text())));
+        }, preg_split("/\r?\n/", trim($node->text())))).$newLine;
     }
 
     public static function filterStyle(Node $node, $indent, $newLine)
