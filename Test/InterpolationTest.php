397d668bc532f0cde578a81a4a0af43148d109aa
Torben Köhn <t.koehn@outlook.com>
2016-02-01 21:07:04 +0100

    More interpolation tests

--- Test/InterpolationTest.php
+++ Test/InterpolationTest.php
@@ -74,4 +74,20 @@ class InterpolationTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('<p>Some text <?=htmlentities(isset($var) ? $var : \'\', \ENT_QUOTES, \'UTF-8\')?> <p>A</p> <?=htmlentities(strtolower($var), \ENT_QUOTES, \'UTF-8\')?> <?=htmlentities(isset($otherVar) ? $otherVar : \'\', \ENT_QUOTES, \'UTF-8\')?> <?=isset($otherVar) ? $otherVar : \'\'?></p>', $this->_renderer->compile("p.\n\tSome text #{\$var} #[p A] #{strtolower(\$var)} #{\$otherVar} !{\$otherVar}"));
         $this->assertEquals('<p>Some text <a>A</a> <p>B</p> <?=htmlentities(isset($var) ? $var : \'\', \ENT_QUOTES, \'UTF-8\')?> <c class="d">E</c> <?=isset($otherVar) ? $otherVar : \'\'?> <f></f></p>', $this->_renderer->compile("p.\n\tSome text #[a A] #[p B] #{\$var} #[c.d E] !{\$otherVar} #[f]"));
     }
+
+    public function testInvalidInterpolation()
+    {
+
+        $this->setExpectedException(Compiler\Exception::class);
+
+        $this->_renderer->compile('#{p Some content');
+    }
+
+    public function testInvalidJadeInterpolation()
+    {
+
+        $this->setExpectedException(Compiler\Exception::class);
+
+        $this->_renderer->compile('#[p Some content');
+    }
 }
